ba9af64ee1d67588649afbd19f5ed0fc66a318f1
halilakaytaydas <halilakaytaydas@hotmail.com.tr>
2017-10-22 23:51:00 +0300

    Initial Commit

---
+++ JSON Parser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSON Parser
+//
+//  Created by halil akay taydaş on 22/10/2017.
+//  Copyright © 2017 THeAkAy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
